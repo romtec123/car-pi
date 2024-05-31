eb59a24a7077b29dc38b66a0c48fb73c5827be9a
romtec123 <julianlevin11@gmail.com>
2024-05-31 15:07:15 -0700

    minor changes to watchdog

--- doorWatchDog.js
+++ doorWatchDog.js
@@ -12,23 +12,31 @@ const config = await getConfig("dw", defaultConfig)
 const doorSensor = new Gpio(config.gpioPin, 'in', 'both'); // GPIO pin 17, change as needed.
 // Watching for any change on the pin, i am a using an optocoupler with the 12v door switch circut
 
-
-
-let doorOpened = false;
+let doorValue = 1; // 1 = Closed, 0 = open
 // Function to handle door open/close events
 doorSensor.watch((err, value) => {
+
     if (err) {
         console.error('Error reading GPIO pin:', err);
         return;
     }
 
-    if(doorOpened) return;
+    if (value == 1) {
 
-    doorOpened = true
-    console.log("Door opened! value: " + value + " Date: " + new Date());
-    setTimeout(() => {
-        doorOpened = false
-    }, config.deBounceWait)
+        if(doorValue == 0){
+            doorValue = 1
+            console.log("Door closed!" + new Date())
+        }
+
+    } else {
+
+        if(doorValue == 1) {
+            doorValue = 0
+            console.log("Door Opened! Time: " + new Date())
+        }
+
+    }
+
 });
 
 // Clean up on exit
